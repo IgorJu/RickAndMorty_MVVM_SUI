b54daf0707cfdc36ffa892f8b32225414f4a0c89
Igor Rukaleev <575714470@mail.ru>
2023-07-14 14:38:04 +0300

    Initial Commit

---
+++ RickAndMorty_MVVM_SUI/RickAndMorty_MVVM_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMorty_MVVM_SUIApp.swift
+//  RickAndMorty_MVVM_SUI
+//
+//  Created by Igor on 14.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMorty_MVVM_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
